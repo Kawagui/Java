ae8a2996a8cbf9d27eafb576d4f4345416d96de6
Kawagui <135202943+Kawagui@users.noreply.github.com>
2024-02-26 19:40:46 -0300

    Create RascunhoClasse.java

---
+++ RascunhoClasse.java
@@ -0,0 +1,11 @@
+public class AClasse {
+	//Tipo dos atributos e permissões
+	
+	//Getters e setters(Source/Shift+Alt+S)
+	
+	/*Construtor
+	 * Nome da classe(parâmetros se vazio atribuir depois se não){
+	 * this.nomeAtributo=nomeAtributo
+	 * }*/
+	//Métodos conforme necessário
+}
